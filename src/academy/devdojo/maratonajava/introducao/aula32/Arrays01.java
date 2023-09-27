f5ad5876ca7f07c923748ab57f95f18e12927cde
rafaelcitario <contato.rafaelgomes@outlook.com>
2023-09-26 21:58:28 -0300

    feat: array02 aula 33

--- src/academy/devdojo/maratonajava/introducao/aula32/Arrays01.java
+++ src/academy/devdojo/maratonajava/introducao/aula32/Arrays01.java
@@ -1,17 +1,14 @@
-package academy.devdojo.maratonajava.arrays.aula32;
+package academy.devdojo.maratonajava.introducao.aula32;
 
 public class Arrays01 {
     public static void main(String[] args) {
         String[] names = {"Rafael", "Leigislane", "Gatinhas"};
-
         int[] ages = new int[3];
-        ages[0] = 15;
+        ages[0] = 10;
         ages[1] = 20;
-        ages[2] = 50;
+        ages[2] = 60;
 
+        System.out.println(ages[0]);
 
-        for (String name : names) {
-            System.out.println("Nome: " + name);
-        }
     }
 }
---
+++ src/academy/devdojo/maratonajava/introducao/aula32/Arrays02.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.introducao.aula32;
+
+public class Arrays02 {
+    public static void main(String[] args) {
+        System.out.println("Array02");
+    }
+}
