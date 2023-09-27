d125777fb9cee617a67d3410f679ca25b6a9560b
rafaelcitario <contato.rafaelgomes@outlook.com>
2023-09-26 23:16:25 -0300

    feat: arrays03 aula 35

--- src/academy/devdojo/maratonajava/introducao/aula35/Arrays03.java
+++ src/academy/devdojo/maratonajava/introducao/aula35/Arrays03.java
@@ -2,6 +2,11 @@ package academy.devdojo.maratonajava.introducao.aula35;
 
 public class Arrays03 {
     public static void main(String[] args) {
+        int[] numeros = {1, 2, 6, 4, 2, 3, 10};
 
+        for (int numero : numeros) {
+            System.out.println(numero);
+        }
+        System.out.println(numeros.length);
     }
 }
